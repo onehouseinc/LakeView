7e324bbc29ed6e890ae6658b588e5adf86a22f0f
sampansnayak <sampansnayak2@gmail.com>
2023-11-05 00:26:38 +0530

    add tests for hudi properties reader

---
+++ src/test/java/com/onehouse/metadataExtractor/HoodiePropertiesReaderTest.java
@@ -0,0 +1,41 @@
+package com.onehouse.metadataExtractor;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import com.onehouse.api.request.TableType;
+import com.onehouse.metadataExtractor.models.ParsedHudiProperties;
+import com.onehouse.storage.AsyncStorageClient;
+import java.io.ByteArrayInputStream;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class HoodiePropertiesReaderTest {
+
+  @Mock private AsyncStorageClient asyncStorageClient;
+
+  @InjectMocks private HoodiePropertiesReader hoodiePropertiesReader;
+
+  @Test
+  void shouldReadHoodieProperties() throws ExecutionException, InterruptedException {
+    String path = "some/path/to/properties/file";
+    String propertiesContent = "hoodie.table.name=test_table\nhoodie.table.type=COPY_ON_WRITE";
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(propertiesContent.getBytes());
+
+    when(asyncStorageClient.readFileAsInputStream(path))
+        .thenReturn(CompletableFuture.completedFuture(inputStream));
+
+    CompletableFuture<ParsedHudiProperties> futureResult =
+        hoodiePropertiesReader.readHoodieProperties(path);
+
+    ParsedHudiProperties result = futureResult.get();
+    assertEquals("test_table", result.getTableName());
+    assertEquals(TableType.COPY_ON_WRITE, result.getTableType());
+  }
+}
