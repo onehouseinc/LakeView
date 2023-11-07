3d22b9136ab27504011b1a5f14f0953d0aa6ac48
sampansnayak <sampansnayak2@gmail.com>
2023-11-07 13:28:00 +0530

    print table details in logs

--- src/main/java/com/onehouse/metadata_extractor/models/Table.java
+++ src/main/java/com/onehouse/metadata_extractor/models/Table.java
@@ -4,10 +4,12 @@ import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.ToString;
 
 @Builder(toBuilder = true)
 @Getter
 @EqualsAndHashCode
+@ToString
 public class Table {
   @NonNull private final String absoluteTableUri;
   @NonNull private final String databaseName;
