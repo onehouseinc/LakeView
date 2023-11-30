ba438440bb68d23d461448774c192bfcf83b7763
sampansnayak <sampansnayak2@gmail.com>
2023-11-30 19:22:15 +0530

    fix lombok annotations

--- src/main/java/com/onehouse/api/models/response/InitializeTableMetricsCheckpointResponse.java
+++ src/main/java/com/onehouse/api/models/response/InitializeTableMetricsCheckpointResponse.java
@@ -1,17 +1,23 @@
 package com.onehouse.api.models.response;
 
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
 @Builder
-@Value
-@Jacksonized
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class InitializeTableMetricsCheckpointResponse extends ApiResponse {
   @Builder
-  @Value
-  @Jacksonized
+  @Getter
+  @AllArgsConstructor
+  @NoArgsConstructor
   public static class InitializeSingleTableMetricsCheckpointResponse {
     String tableId;
     String error;
