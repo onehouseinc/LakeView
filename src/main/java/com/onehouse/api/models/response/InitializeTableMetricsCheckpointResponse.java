024ef765551e30630d50c51f967ce9f108a1d14f
sampansnayak <sampansnayak2@gmail.com>
2023-11-30 19:39:03 +0530

    spotless apply

--- src/main/java/com/onehouse/api/models/response/InitializeTableMetricsCheckpointResponse.java
+++ src/main/java/com/onehouse/api/models/response/InitializeTableMetricsCheckpointResponse.java
@@ -1,13 +1,10 @@
 package com.onehouse.api.models.response;
 
 import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Value;
-import lombok.extern.jackson.Jacksonized;
 
 @Builder
 @Getter
