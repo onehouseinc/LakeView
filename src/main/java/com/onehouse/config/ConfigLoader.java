5567c1ab21d631dba05c8346f71fb110dfe7aab8
Praveen Gajulapalli <13733716+pkgajulapalli@users.noreply.github.com>
2024-03-12 17:46:35 +0530

    feat: load onehouse client configuration from file is provided

--- src/main/java/com/onehouse/config/ConfigLoader.java
+++ src/main/java/com/onehouse/config/ConfigLoader.java
@@ -5,10 +5,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.onehouse.config.models.common.OnehouseClientConfig;
 import com.onehouse.config.models.configv1.ConfigV1;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import org.apache.commons.lang3.StringUtils;
 
 public class ConfigLoader {
   private final ObjectMapper MAPPER;
@@ -34,11 +37,27 @@ public class ConfigLoader {
     }
   }
 
-  private Config loadConfigFromJsonNode(JsonNode jsonNode) throws JsonProcessingException {
+  private Config loadConfigFromJsonNode(JsonNode jsonNode) throws IOException {
     ConfigVersion version = ConfigVersion.valueOf(jsonNode.get("version").asText());
     switch (version) {
       case V1:
-        return MAPPER.treeToValue(jsonNode, ConfigV1.class);
+        ConfigV1 configV1 = MAPPER.treeToValue(jsonNode, ConfigV1.class);
+        if (StringUtils.isNotEmpty(configV1.getOnehouseClientConfig().getFile())) {
+          String onehouseClientConfigFileContent =
+              new String(
+                  Files.readAllBytes(Paths.get(configV1.getOnehouseClientConfig().getFile())));
+          OnehouseClientConfig onehouseClientConfigFromFile =
+              MAPPER.readValue(onehouseClientConfigFileContent, OnehouseClientConfig.class);
+          configV1
+              .getOnehouseClientConfig()
+              .setProjectId(onehouseClientConfigFromFile.getProjectId());
+          configV1.getOnehouseClientConfig().setApiKey(onehouseClientConfigFromFile.getApiKey());
+          configV1
+              .getOnehouseClientConfig()
+              .setApiSecret(onehouseClientConfigFromFile.getApiSecret());
+          configV1.getOnehouseClientConfig().setUserId(onehouseClientConfigFromFile.getUserId());
+        }
+        return configV1;
       default:
         throw new UnsupportedOperationException("Unsupported config version: " + version);
     }
