045d4feadf3b05613fbe92320105f7e9a3b4282f
sampansnayak <sampansnayak2@gmail.com>
2024-05-30 13:25:40 +0530

    spotless apply

--- src/main/java/com/onehouse/metrics/MetricsServer.java
+++ src/main/java/com/onehouse/metrics/MetricsServer.java
@@ -4,10 +4,13 @@ import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class MetricsServer {
   public MetricsServer(CollectorRegistry registry, int port) {
     try (HTTPServer server = initHttpServer(new InetSocketAddress(port), registry)) {
+      log.info("Starting metrics server");
       Runtime.getRuntime().addShutdownHook(new Thread(server::close));
     } catch (IOException e) {
       throw new RuntimeException("Failed to start Prometheus server", e);
