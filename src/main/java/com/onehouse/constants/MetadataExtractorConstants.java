ce559f4caa64fb2d3f3355d079cdc7540ea3c7df
Praveen Gajulapalli <13733716+pkgajulapalli@users.noreply.github.com>
2024-08-30 15:41:02 +0530

    Use optional env variable for stream upload batch size

--- src/main/java/com/onehouse/constants/MetadataExtractorConstants.java
+++ src/main/java/com/onehouse/constants/MetadataExtractorConstants.java
@@ -2,13 +2,15 @@ package com.onehouse.constants;
 
 import com.onehouse.metadata_extractor.models.Checkpoint;
 import com.onehouse.storage.models.File;
+
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class MetadataExtractorConstants {
-  private MetadataExtractorConstants() {}
+  private MetadataExtractorConstants() {
+  }
 
   public static final String HOODIE_FOLDER_NAME = ".hoodie";
   public static final String ARCHIVED_FOLDER_NAME = "archived";
@@ -32,7 +34,9 @@ public class MetadataExtractorConstants {
               System.getenv().getOrDefault("EXTRACTOR_TABLE_PROCESSING_BATCH_SIZE", "20")));
   public static final int TABLE_DISCOVERY_INTERVAL_MINUTES = 30;
   public static final int TABLE_METADATA_UPLOAD_INTERVAL_MINUTES = 5;
-  public static final int DEFAULT_FILE_UPLOAD_STREAM_BATCH_SIZE = 5 * 1024 * 1024;
+  // Default batch size will be 5 MB
+  public static final int DEFAULT_FILE_UPLOAD_STREAM_BATCH_SIZE = Integer.parseInt(
+      System.getenv().getOrDefault("FILE_UPLOAD_STREAM_BATCH_SIZE", "5242880"));
   public static final Pattern ARCHIVED_COMMIT_INSTANT_PATTERN =
       Pattern.compile("\\.commits_\\.archive\\.\\d+_\\d+-\\d+-\\d+");
   public static final Pattern ACTIVE_COMMIT_INSTANT_PATTERN =
