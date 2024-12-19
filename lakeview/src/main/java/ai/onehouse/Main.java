6fee5b6bf42118d3cfc393e02783b241763596de
Karan Mittal <karan@onehouse.ai>
2024-12-19 18:30:10 +0530

    removed unused imports

--- lakeview/src/main/java/ai/onehouse/Main.java
+++ lakeview/src/main/java/ai/onehouse/Main.java
@@ -19,9 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 @Slf4j
 public class Main {
 
