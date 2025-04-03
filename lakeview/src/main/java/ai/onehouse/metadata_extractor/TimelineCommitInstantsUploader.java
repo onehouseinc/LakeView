4b9846f7fdb8c4cacb0f7c2028316fb33f493b2a
Karan Mittal <karan@onehouse.ai>
2025-04-04 00:42:48 +0530

    Revert "More changes for testing"
    This reverts commit 30350105fdecd6178b28e6ffdf97bbdc0746bcbb.

--- lakeview/src/main/java/ai/onehouse/metadata_extractor/TimelineCommitInstantsUploader.java
+++ lakeview/src/main/java/ai/onehouse/metadata_extractor/TimelineCommitInstantsUploader.java
@@ -209,16 +209,16 @@ public class TimelineCommitInstantsUploader {
                             hudiMetadataExtractorMetrics.incrementTablesProcessedCounter();
                             return CompletableFuture.completedFuture(checkpoint);
                           }
-//                          if (StringUtils.isBlank(nextContinuationToken)) {
-//                            log.info(
-//                                "Reached end of instants in {} for table {}",
-//                                commitTimelineType,
-//                                table);
-//                            hudiMetadataExtractorMetrics.incrementTablesProcessedCounter();
-//                            return CompletableFuture.completedFuture(updatedCheckpoint);
-//                          }
+                          if (StringUtils.isBlank(nextContinuationToken)) {
+                            log.info(
+                                "Reached end of instants in {} for table {}",
+                                commitTimelineType,
+                                table);
+                            hudiMetadataExtractorMetrics.incrementTablesProcessedCounter();
+                            return CompletableFuture.completedFuture(updatedCheckpoint);
+                          }
                           log.info(
-                              "Uploading more instants in {} for table {}",
+                              "Uploading instants in {} for table {}",
                               commitTimelineType,
                               table);
                           try {
